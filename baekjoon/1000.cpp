c6b751545a77efa11e0243f889346ce876560493
dahyeonlii <dahyeon97@naver.com>
2023-01-24 02:22:09 +0900

    BEAKJOON No.1000 complete

---
+++ baekjoon/1000.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+    
+    int A, B;
+
+    scanf("%d %d", &A, &B);
+    printf("%d", A+B);
+
+    return 0;
+
+}
+
+
+/*
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+    int A, B;
+
+    cin >> A;
+    cin >> B;
+
+    cout << A+B;
+
+    return 0;
+
+}
+*/

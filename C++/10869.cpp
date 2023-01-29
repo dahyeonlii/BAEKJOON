4b1f4d575afe64947eb98166375c1a1cae77e706
dahyeonlii <dahyeon97@naver.com>
2023-01-30 00:09:07 +0900

    BAEKJOON No.10869 complete

---
+++ C++/10869.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file 10869.cpp
+ * @author Dahyeon Lee (https://github.com/dahyeonlii)
+ * @brief BAEKJOON Algorithm 100869 using C++
+ * @version 0.1
+ * @date 2023-01-30
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+
+#include <iostream>
+
+int main(int argc, char const *argv[]){
+
+    using namespace std;
+
+    int A, B;
+    
+    cin >> A;
+    cin >> B;
+
+    cout << A+B << endl;
+    cout << A-B << endl;
+    cout << A*B << endl;
+    cout << A/B << endl;
+    cout << A%B;
+
+}
+
---
+++ C/10869.c
@@ -0,0 +1,28 @@
+/**
+ * @file 10869.c
+ * @author Dahyeon Lee (https://github.com/dahyeonlii)
+ * @brief BAEKJOON Algorithm 10869 using C
+ * @version 0.1
+ * @date 2023-01-30
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+
+#include <stdio.h>
+
+int main(){
+
+    int A, B;
+
+    scanf("%d %d", &A, &B);
+
+    printf("%d\n", A+B);
+    printf("%d\n", A-B);
+    printf("%d\n", A*B);
+    printf("%d\n", A/B);
+    printf("%d", A%B);
+
+}
+

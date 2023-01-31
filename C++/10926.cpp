e41128946866e867a791999e6a1db747145c05d3
dahyeonlii <dahyeon97@naver.com>
2023-01-31 12:45:58 +0900

    BAEKJOON No.10926 complete

---
+++ C++/10926.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file 10926.cpp
+ * @author Dahyeon Lee (https://github.com/dahyeonlii)
+ * @brief BAEKJOON Algorithm 10869 using C++
+ * @version 0.1
+ * @date 2023-01-31
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+
+    string inputId;
+
+    cin >> inputId;
+    cout << inputId + "\?\?!";
+
+}
---
+++ C/10926.c
@@ -0,0 +1,24 @@
+/**
+ * @file 10926.c
+ * @author Dahyeon Lee (https://github.com/dahyeonlii)
+ * @brief BAEKJOON Algorithm 10869 using C
+ * @version 0.1
+ * @date 2023-01-31
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+
+#include <stdio.h>
+
+int main(){
+
+    char inputId[50];
+    
+    scanf("%s", inputId);
+    printf("%s\?\?!", inputId);
+
+    return 0;
+
+}

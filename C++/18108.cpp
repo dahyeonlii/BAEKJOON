93bd02eea40c7ad4f4f1ec6319c45beb655ee9fd
dahyeonlii <dahyeon97@naver.com>
2023-02-03 22:17:48 +0900

    BAEKJOON No.18108 complete

---
+++ C++/18108.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file 18108.cpp
+ * @author Dahyeon Lee (https://github.com/dahyeonlii)
+ * @brief BAEKJOON Algorithm 18108 using C
+ * @version 0.1
+ * @date 2023-02-03
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+
+#include <iostream>
+
+int main(int argc, char const *argv[]){
+
+    using namespace std;
+
+    int year;
+
+    cin >> year;
+    cout << year - 543;
+
+    return 0;
+
+}
---
+++ C/18108.c
@@ -0,0 +1,24 @@
+/**
+ * @file 18108.c
+ * @author Dahyeon Lee (https://github.com/dahyeonlii)
+ * @brief BAEKJOON Algorithm 18108 using C
+ * @version 0.1
+ * @date 2023-02-03
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+
+#include <stdio.h>
+
+int main(){
+
+    int year;
+
+    scanf("%d", &year);
+    printf("%d", year-(int)543);
+
+    return 0;
+
+}

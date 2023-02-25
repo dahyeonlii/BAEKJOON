278bf0166ae47c4846daf4b637e7eeffbfd87906
dahyeonlii <dahyeon97@naver.com>
2023-02-25 22:53:57 +0900

    BAEKJOON No.10172 complete

---
+++ C++/10172.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file 10172.cpp
+ * @author Dahyeon Lee (https://github.com/dahyeonlii)
+ * @brief BAEKJOON Algorithm 10172 using C++
+ * @version 0.1
+ * @date 2023-02-25
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    
+    cout << "|\\_/|" << endl;
+    cout << "|q p|   /}" << endl;
+    cout << "( 0 )\"\"\"\\" << endl;
+    cout << "|\"^\"`    |" << endl;
+    cout << "||_/=\\\\__|" << endl;
+
+    return 0; 
+}
+
---
+++ C/10172.c
@@ -0,0 +1,32 @@
+/**
+ * @file 10172.c
+ * @author Dahyeon Lee (https://github.com/dahyeonlii)
+ * @brief BAEKJOON Algorithm 10172 using C
+ * @version 0.1
+ * @date 2023-02-25
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <stdio.h>
+
+int main(){
+
+    char s1[10] = "|\\_/|";
+    char s2[20] = "|q p|   /}";
+    char s3[20] = "( 0 )\"\"\"\\";
+    char s4[20] = "|\"^\"`    |";
+    char s5[20] = "||_/=\\\\__|";
+    
+    //printf("%s", s1);
+    puts(s1);
+    puts(s2);
+    puts(s3);
+    puts(s4);
+    puts(s5);
+
+    return 0;
+
+}
+
